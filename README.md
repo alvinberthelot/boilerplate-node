8ba9e4f507c0d7d3a2cee39a75b8c7bb447ad0a9
Alvin BERTHELOT <alvin.berthelot@webyousoon.com>
2016-05-13 00:06:18 +0200

    🌱  Install mochai and chai with first test

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+[Node.js](https://nodejs.org)
+
+	npm i
---
+++ test/basic-tests.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+ // we are using the "expect" style of Chai
+var expect = chai.expect;
+
+describe('Basic tests', function() {
+
+  it('true is true (of course)', function() {
+    expect(true).to.be.true;
+  });
+
+});
